420ddd20c846bf5c8ebca0258ab79c839333e3de
Mariela Oliveira <mroliver026@gmail.com>
2025-04-15 16:46:55 -0300

    sintaxis if agrego if.py

---
+++ if.py
@@ -0,0 +1,13 @@
+edad = 25
+if edad < 13:
+ 
+ print("Sos menor a trece años.")
+elif edad < 18:
+ 
+ print("Sos un o una adolescente.")
+elif edad < 60:
+ 
+ print("Sos una persona adulta.")
+else:
+ 
+ print("Sos una persona adulta mayor.")
